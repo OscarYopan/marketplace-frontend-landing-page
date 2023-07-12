78657fd0938314812cc3da9becd22f9571c81547
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-12 15:40:49 -0300

    --

---
+++ src/components/NavMenu/NavMenu.jsx
@@ -0,0 +1,16 @@
+import './navMenu.css'
+
+export const NavMenu = () => {
+  return (
+    <div className="nav">
+      <ul className="menu">
+        <li>Games</li>
+        <li>Computer</li>
+        <li>Eletronic</li>
+        <li>Toys</li>
+        <li>Home</li>
+        <li>Pet</li>
+      </ul>
+    </div>
+  )
+}
