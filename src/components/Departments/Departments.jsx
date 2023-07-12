f4eac9d5f66e56f8042c238bc604d76db8d83dd3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-12 15:32:05 -0300

    --

---
+++ src/components/Departments/Departments.jsx
@@ -0,0 +1,33 @@
+
+import './departments.css'
+
+export const Departments = () => {
+  return (
+    <div className="departments">
+      <div className="department" id='games-dep'>
+        <h2 className="department-title">Games</h2>
+      </div>
+
+      <div className="department" id='computer-dep'>
+        <h2 className="department-title">Computer</h2>
+      </div>
+
+      <div className="department" id='eletronic-dep'>
+        <h2 className="department-title">Eletronic</h2>
+      </div>
+
+      <div className="department" id='toys-dep'>
+        <h2 className="department-title">Toys</h2>
+      </div>
+
+      <div className="department" id='home-dep'>
+        <h2 className="department-title">Home</h2>
+      </div>
+
+      <div className="department" id='pet-dep'>
+        <h2 className="department-title">Pet</h2>
+      </div>
+      
+    </div>
+  )
+}
