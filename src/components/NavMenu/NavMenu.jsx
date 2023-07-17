87f183c3a2b9cf422c9f941a15908464617629e3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-17 17:04:26 -0300

    Update NavMenu.jsx

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,17 +1,27 @@
 import { GamesMenu } from './SubMenus/GamesMenu/GamesMenu'
+import { useState } from 'react';
 import './navMenu.css'
 
 export const NavMenu = () => {
+  const [gameShow, setGameShow] = useState(false);
+
+  const handleClickGames = () => {
+    setGameShow(current => !current);
+  };
+
   return (
     <div className="nav">
       <ul className="menu">
-        <li>Games</li>
+        <li onMouseEnter={handleClickGames} onMouseLeave={handleClickGames}>
+          Games
+        </li>
         <li>Computer</li>
         <li>Eletronic</li>
         <li>Toys</li>
         <li>Home</li>
         <li>Pet</li>
       </ul>
+      {gameShow && <GamesMenu />}
     </div>
   )
 }
