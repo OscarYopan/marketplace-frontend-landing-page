f5d6586f1a30baead0114d4f2afad0f833669f1a
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-17 16:51:22 -0300

    Create GamesMenu.jsx

---
+++ src/components/NavMenu/SubMenus/GamesMenu/GamesMenu.jsx
@@ -0,0 +1,12 @@
+export const GamesMenu = () => {
+  return (
+    <div>
+      <ul>
+        <li>Video Game</li>
+        <li>Gamepad</li>
+        <li>Accessories</li>
+        <li>Simulators</li>
+      </ul>
+    </div>
+  )
+}
