2835863ef298bc02f7bb800422ab2c649fa6e515
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-18 16:29:36 -0300

    --

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,12 +1,19 @@
 import { GamesMenu } from './SubMenus/GamesMenu/GamesMenu'
 import { useState } from 'react';
+import { ComputerMenu } from './SubMenus/Computer/ComputerMenu';
 import './navMenu.css'
 
 export const NavMenu = () => {
   const [gameShow, setGameShow] = useState(false);
+  const [computerShow, setComputerShow] = useState(false);
+
 
   const handleClickGames = () => {
     setGameShow(current => !current);
+  };
+
+  const handleClickComputer = () => {
+    setComputerShow(current => !current);
   };
 
   return (
@@ -18,13 +25,19 @@ export const NavMenu = () => {
         >
           Games
         </li>
-        <li>Computer</li>
+        <li 
+          onMouseEnter={handleClickComputer} 
+          onMouseLeave={handleClickComputer}
+        >
+          Computer
+        </li>
         <li>Eletronic</li>
         <li>Toys</li>
         <li>Home</li>
         <li>Pet</li>
       </ul>
       {gameShow && <GamesMenu />}
+      {computerShow && <ComputerMenu />}
     </div>
   )
 }
