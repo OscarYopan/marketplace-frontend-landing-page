6d75d88e078d21d115e198f5666b9b7d0b5dfe30
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-18 16:17:08 -0300

    --

---
+++ src/components/NavMenu/SubMenus/Computer/ComputerMenu.jsx
@@ -0,0 +1,44 @@
+export const ComputerMenu = () => {
+  return (
+    <div className="sub-menu">
+      <ul>
+      <li 
+          // onMouseEnter={handleClickGames} 
+          // onMouseLeave={handleClickGames}
+        >
+          Games
+        </li>
+        <li 
+          // onMouseEnter={handleClickGames} 
+          // onMouseLeave={handleClickGames}
+        >
+          Games
+        </li>
+        <li 
+          // onMouseEnter={handleClickGames} 
+          // onMouseLeave={handleClickGames}
+        >
+          Games
+        </li>
+        <li 
+          // onMouseEnter={handleClickGames} 
+          // onMouseLeave={handleClickGames}
+        >
+          Games
+        </li>
+        <li 
+          // onMouseEnter={handleClickGames} 
+          // onMouseLeave={handleClickGames}
+        >
+          Games
+        </li>
+        <li 
+          // onMouseEnter={handleClickGames} 
+          // onMouseLeave={handleClickGames}
+        >
+          Games
+        </li>
+      </ul>
+    </div>
+  )
+}
