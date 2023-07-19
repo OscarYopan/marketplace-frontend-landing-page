d347734020bf9f66d9a00c331b1db66a2a79c53e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-19 16:36:38 -0300

    Create HomeMenu.jsx

---
+++ src/components/NavMenu/SubMenus/HomeMenu/HomeMenu.jsx
@@ -0,0 +1,14 @@
+export const HomeMenu = () => {
+  return (
+    <div className="sub-menu home-menu">
+      <ul>
+        <li>Tables</li>
+        <li>Chairs</li>
+        <li>Cookwares</li>
+        <li>Dineer Sets</li>
+        <li>kitchenware</li>
+        <li>Cutlery</li>
+      </ul>
+    </div>
+  )
+}
