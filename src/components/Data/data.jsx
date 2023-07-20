12ed50af5a9ba7698fcb3a48dc1517d88ef67b9c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-20 19:18:55 -0300

    --

--- src/components/Data/data.jsx
+++
@@ -1,5 +0,0 @@
-export const bannerImages = [
-  { id: 1, image: banner01, text: 'banner 01' },
-  { id: 1, image: banner02, text: 'banner 02' },
-  { id: 1, image: banner03, text: 'banner 03' },  
-];
