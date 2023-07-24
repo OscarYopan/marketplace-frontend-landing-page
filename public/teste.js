cb70057c97de0ec2610534e682a335a2435b7e8f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-24 17:24:16 -0300

    Update teste.js

--- public/teste.js
+++ public/teste.js
@@ -4,15 +4,17 @@ fetch('https://fakestoreapi.com/products')
   .then(res => res.json())
   .then(json => console.log(json))
 
+createProductAnunce()
+
 function createProductAnunce() {
   const productAnunce = document.querySelector('.product-container')
 
   productAnunce.innerHTML = `
-  <div class="anunce">
-    <img src="" alt="" class="image-product">
-    <h2 class="title-product"></h2>
-    <p class="description"></p>
-    <span class="price"></span>
-  </div>
+    <div class="anunce">
+      <img src="" alt="" class="image-product">
+      <h2 class="title-product"></h2>
+      <p class="description"></p>
+      <span class="price"></span>
+    </div>
   `
 }
