a2e6a7f2bbef5b2e019da28a41469dcead1291a5
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-24 18:15:29 -0300

    --

--- src/components/Data/data.jsx
+++ src/components/Data/data.jsx
@@ -2,7 +2,9 @@ import product00 from '../../assets/images/product_00.jpg'
 import product01 from '../../assets/images/product_01.jpg'
 import product02 from '../../assets/images/product_02.jpg'
 import product03 from '../../assets/images/product_03.jpg'
-
+import product04 from '../../assets/images/product_04.jpg'
+import product05 from '../../assets/images/product_05.jpg'
+import product06 from '../../assets/images/product_06.jpg'
 
 export const data = [
   {
@@ -36,6 +38,30 @@ export const data = [
     description: 'Apple’s thinnest and lightest notebook gets supercharged with the Apple M1 chip. Tackle your projects with the blazing-fast 8-core CPU. Take graphics-intensive apps and games to the next level with the 7-core GPU. And accelerate machine learning tasks with the 16-core Neural Engine. All with a silent, fanless design and the longest battery life ever — up to 18 hours.¹ MacBook Air. Still perfectly portable. Just a lot more powerful.',
     category: 'Computer',
     Image: product03,
+  },
+  {
+    id: 4,
+    tite: 'Samsung - Galaxy Watch5 Aluminum Smartwatch',
+    price: '199.99',
+    description: 'Stay on track, on time, and in style with Galaxy Watch5¹. Ready to get a better understanding of your wellness? Galaxy Watch5 features body composition analysis², improved sensors that get an accurate read on your heart rate³ and Advanced Sleep Coaching¹ that helps you get your zzzs . Whether you’re getting your steps in, jogging at lunchtime or crushing yoga class on the weekend, strut your fashion sense with Watch bands and faces that match your awesome outfit. Stay powered up on the go throughout even your busiest day with an improved battery that gives you time to do it all. Worry less and do more and with a Sapphire Crystal Glass that’s more durable than before and a water resistant design that can take a splash⁴ or two. Go ahead, unleash the best you with Galaxy Watch5.',
+    category: 'Eletronic',
+    Image: product04,
+  },
+  {
+    id: 5,
+    tite: 'Canon - EOS Rebel T7 DSLR Video Two Lens Kit',
+    price: '549.99',
+    description: 'Take stunning pictures with vibrant colors using this EOS Rebel T7 camera. The built-in Wi-Fi makes printing and sharing pictures quick and easy, and the auto-focus system creates crisp images at a moments notice. This EOS Rebel T7 camera takes Full HD video, allowing experiences to be captured with quality.',
+    category: 'Eletronic',
+    Image: product05,
+  },
+  {
+    id: 6,
+    tite: 'Blue Microphones - Blue Yeti Professional Multi-Pattern USB Condenser Microphone',
+    price: '109.99',
+    description: 'Yeti is the premium USB mic professionals trust worldwide, producing clear, powerful, broadcast-quality sound and enhanced Blue VO!CE vocal effects for gaming, streaming, podcasting, YouTube videos and more. Thanks to Yeti’s proprietary three-capsule technology and four pickup patterns, you can produce professional recordings and streams with legendary ease. Yeti also features studio controls for headphone volume, pattern selection, instant mute, and microphone gain that put you in chargeof the recording process. Set up in seconds with the included desktop stand or connect directly to a mic stand or boom arm.',
+    category: 'Computer',
+    Image: product06,
   },
   {
     id: 0,
