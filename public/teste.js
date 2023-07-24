b55de7fafe5091a6de4c0743ac1d0e12722fb092
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-24 16:59:40 -0300

    Update teste.js

--- public/teste.js
+++ public/teste.js
@@ -1,1 +1,11 @@
 console.log('Open Teste API')
+
+dataImport()
+
+function dataImport() {
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(json => console.log(json))
+}
+
+// https://fakestoreapi.com/docs
