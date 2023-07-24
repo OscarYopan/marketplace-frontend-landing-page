0be020671079f5e150d04d3cd95c215078224bf2
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-24 17:51:27 -0300

    --

--- src/components/Data/data.jsx
+++ src/components/Data/data.jsx
@@ -1,15 +1,29 @@
-  export const Data = () => {
+import product00 from '../../assets/images/product_00.jpg'
 
-    function data() {
-      fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>console.log(json))
-    }
 
-    return (
-      <div>{data}</div>
-    )
-  }
-
-  // https://fakestoreapi.com/docs
-  
+export const data = [
+  {
+    id: 0,
+    title: 'GoPro - HERO11 Black Mini',
+    price: '199.90',
+    description: 'Get incredible HERO11 Black video performance in a smaller, lighter, simpler design. HERO11 Black Mini’s size makes it more comfortable to wear when capturing point-of-view footage, while its new dual mounting fingers boost your body and helmet mounting options. As waterproof as ever and even more rugged, it’s ready for any adventure. Simply press the Shutter button to power up and start recording. Its new image sensor ensures you’ll get stunning video up to 5.3K with silky-smooth HyperSmooth 5.0 video stabilization.',
+    category: 'Eletronic',
+    Image: product00,
+  },
+  {
+    id: 1,
+    tite: 0,
+    price: 0,
+    description: 0,
+    category: 0,
+    Image: 0,
+  },
+  {
+    id: 0,
+    tite: 0,
+    price: 0,
+    description: 0,
+    category: 0,
+    Image: 0,
+  },
+]
