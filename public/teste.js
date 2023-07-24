636e79e8f4342dc37e20e265922173853c908cf7
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-24 17:12:33 -0300

    --

--- public/teste.js
+++ public/teste.js
@@ -1,11 +1,18 @@
 console.log('Open Teste API')
 
-dataImport()
+fetch('https://fakestoreapi.com/products')
+  .then(res => res.json())
+  .then(json => console.log(json))
 
-function dataImport() {
-  fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(json => console.log(json))
+function createProductAnunce() {
+  const productAnunce = document.querySelector('.product-container')
+
+  productAnunce.innerHTML = `
+  <div class="anunce">
+    <img src="" alt="" class="image-product">
+    <h2 class="title-product"></h2>
+    <p class="description"></p>
+    <span class="price"></span>
+  </div>
+  `
 }
-
-// https://fakestoreapi.com/docs
