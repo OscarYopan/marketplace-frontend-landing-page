2812e83fa4adfd66260778336f1861c48733dac6
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-24 18:03:07 -0300

    --

--- src/components/Data/data.jsx
+++ src/components/Data/data.jsx
@@ -1,4 +1,7 @@
 import product00 from '../../assets/images/product_00.jpg'
+import product01 from '../../assets/images/product_01.jpg'
+import product02 from '../../assets/images/product_02.jpg'
+import product03 from '../../assets/images/product_03.jpg'
 
 
 export const data = [
@@ -12,11 +15,27 @@ export const data = [
   },
   {
     id: 1,
-    tite: 0,
-    price: 0,
-    description: 0,
-    category: 0,
-    Image: 0,
+    tite: 'Sony - PlayStation 5 Console',
+    price: '499.99',
+    description: 'The PS5 console unleashes new gaming possibilities that you never anticipated. Experience lightning fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers, and 3D Audio*, and an all-new generation of incredible PlayStation games. *3D audio via built-in TV speakers or analog/USB stereo headphones. Set up and latest system software update required. ',
+    category: 'Games',
+    Image: product01,
+  },
+  {
+    id: 2,
+    tite: 'Logitech - G502 HERO Wired Optical Gaming Mouse',
+    price: '48.99',
+    description: 'Step up your game with this Logitech HERO Core wired gaming mouse.Customize RGB lighting to match your style and environment or sync to other Logitech G products. G502 High Performance Gaming Mouse features Logitech HERO 25K optical sensor for maximum tracking accuracy and precision. No matter your gaming style, it’s easyto tweak G502 to match your requirements, with custom profiles for your games, adjustable sensitivity from 200 up to 25,600 DPI, and tunable weight system that allows for tuning and balancing of up to five additional weights for just the right balance andfeel.',
+    category: 'Computer',
+    Image: product02,
+  },
+  {
+    id: 3,
+    tite: 'MacBook Air 13.3" Laptop - Apple M1 chip - 8GB Memory - 256GB SSD - Space Gray',
+    price: '749.99',
+    description: 'Apple’s thinnest and lightest notebook gets supercharged with the Apple M1 chip. Tackle your projects with the blazing-fast 8-core CPU. Take graphics-intensive apps and games to the next level with the 7-core GPU. And accelerate machine learning tasks with the 16-core Neural Engine. All with a silent, fanless design and the longest battery life ever — up to 18 hours.¹ MacBook Air. Still perfectly portable. Just a lot more powerful.',
+    category: 'Computer',
+    Image: product03,
   },
   {
     id: 0,
