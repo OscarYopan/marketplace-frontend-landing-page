e905e3b8afe4e67267f61b0c331eaf9ef34ee7e8
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-27 17:34:17 -0300

    --

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,30 @@ import './footer.css'
 export const Footer = () => {
   return (
     <div className='footer-container'>
-      <div className='socials'></div>
-      <div className='contact'></div>
-      <div className='menu'>
-      <ul className="footer-menu">
-        <li>Games</li>
-        <li>Computer</li>
-        <li>Eletronic</li>
-        <li>Toys</li>
-        <li>Home</li>
-        <li>Pet</li>            
-      </ul>
+      <div className='contact'>
+
       </div>
+
+      <div className='menu'>
+        <ul className="footer-menu">
+          <li>Games</li>
+          <li>Computer</li>
+          <li>Eletronic</li>
+          <li>Toys</li>
+          <li>Home</li>
+          <li>Pet</li>            
+        </ul>
+      </div>
+
+      <div className='socials'>
+        <i className="fa-brands fa-facebook"></i>
+        <i className="fa-brands fa-youtube"></i>
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-tiktok"></i>
+        <i className="fa-brands fa-linkedin"></i>
+      </div>
+
     </div>
   )
 }
