888c127b46e1d671af558243f066eed734151aef
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-27 17:25:51 -0300

    ---

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,19 @@ import './footer.css'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <div className='socials'></div>
+      <div className='contact'></div>
+      <div className='menu'>
+      <ul className="footer-menu">
+        <li>Games</li>
+        <li>Computer</li>
+        <li>Eletronic</li>
+        <li>Toys</li>
+        <li>Home</li>
+        <li>Pet</li>            
+      </ul>
+      </div>
+    </div>
   )
 }
