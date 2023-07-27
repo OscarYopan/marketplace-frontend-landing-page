23a56ea5cf21175e8a31c364371f226cce207ce3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-07-27 17:46:10 -0300

    --

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,23 @@ export const Footer = () => {
   return (
     <div className='footer-container'>
       <div className='contact'>
-
+        <h4>Treatment</h4>
+        <h5>Opening hours:</h5>
+        <p>08:00 to 20:00 -</p>
+        <p>Monday to Saturday,</p>
+        <p>Brasilia time</p>
+        <p>(Except Sunday and holidays)</p>
+        <br />        
+        <h4>Address:</h4>
+        <p>Rua Roronoa Zoro, 1300 -</p>
+        <p>9th floor - Center </p>       
+        <p>Alabast/OP - Zip code: 13480-010</p>
+        <br />      
+        <h4>Central SAC:</h4>
+        <p>(55) 3232-4444</p>
+        <br />
+        <h4>Email:</h4>
+        <p>shopping@shopping.com</p>
       </div>
 
       <div className='menu'>
